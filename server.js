c9f0f935382d486f8b05b2bdca83a6565bf7807f
avm5998 <avm5998@gmail.com>
2020-05-11 17:32:16 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express=require('express');
+
+
+const PORT=process.env.PORT||5000;
+const app=express();
+
+app.get('/',(req,res)=>res.send('API running'));
+app.listen(PORT,()=>console.log(`Server running on ${PORT}`));
